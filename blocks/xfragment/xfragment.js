f77aecd266bd15dd19715d88ecd289ec6ba045aa
Gali Andal <gandal@adobe.com>
2024-03-13 09:53:33 -0400

    poc

---
+++ blocks/xfragment/xfragment.js
@@ -0,0 +1,25 @@
+export async function getFragment(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    // eslint-disable-next-line no-console
+    console.error('error loading fragment details', response);
+    return null;
+  }
+  const text = await response.text();
+  if (!text) {
+    // eslint-disable-next-line no-console
+    console.error('fragment details empty', url);
+    return null;
+  }
+  return text;
+}
+
+
+export default async function decorate(block) {
+  const [xfragmentUrl] = [...block.children].map(
+    (row) => row.firstElementChild,
+  );
+  const text = getFragment(xfragmentUrl);
+  const xfragmentDOM = document.createRange().createContextualFragment(text);
+  block.innerHTML = text;
+}
