07f82a87663eaeac684a95b99393814aa8b63876
gandaltest <159396922+gandaltest@users.noreply.github.com>
2024-03-13 11:15:55 -0400

    Update xfragment.js

--- blocks/xfragment/xfragment.js
+++ blocks/xfragment/xfragment.js
@@ -1,25 +1,18 @@
-export async function getFragment(url) {
-  const response = await fetch(url.querySelector('a').href);
-  if (!response.ok) {
-    // eslint-disable-next-line no-console
-    console.error('error loading fragment details', response);
-    return null;
-  }
-  const text = await response.text();
-  if (!text) {
-    // eslint-disable-next-line no-console
-    console.error('fragment details empty', url);
-    return null;
-  }
-  return text;
-}
+// fetch fragment html
+const fetchFragment = async (url) => {
+  const response = await (url.querySelector('a').href);
+  return response.text();
+};
 
 
 export default async function decorate(block) {
   const [xfragmentUrl] = [...block.children].map(
     (row) => row.firstElementChild,
   );
-  const text = getFragment(xfragmentUrl);
-  const xfragmentDOM = document.createRange().createContextualFragment(text);
-  block.innerHTML = text;
+  const teaserFragment = await fetchFragment(xfragmentUrl);
+  if (teaserFragment) {
+    const xfragmentDOM = document.createRange().createContextualFragment(text);
+    block.innerHTML = teaserFragment;
+  }
+
 }
