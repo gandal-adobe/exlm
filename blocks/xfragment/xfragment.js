d5a0050f5e6d80b5486eda11cd9b985189a8cb9a
gandaltest <159396922+gandaltest@users.noreply.github.com>
2024-03-13 21:29:43 -0400

    Update xfragment.js

--- blocks/xfragment/xfragment.js
+++ blocks/xfragment/xfragment.js
@@ -1,6 +1,6 @@
 import {decorateBlock, loadBlocks} from '../../scripts/lib-franklin.js';
 import { decorateMain } from '../../scripts/scripts.js';
-import decorateXFragment from '../xfragment/xfragment.js';
+import decorateTeaser from '../teaser/teaser.js';
 
 // fetch fragment html
 const fetchFragment = async (url) => {
@@ -19,7 +19,7 @@ export default async function decorate(block) {
     //contentDiv.classList.add('teaser-wrapper');
     const xfragmentDOM = document.createRange().createContextualFragment(teaserFragment);
     //contentDiv.appendChild(xfragmentDOM.querySelector('main').firstElementChild.firstElementChild);
-    const xfragmentBlock = decorateXFragment(xfragmentDOM.querySelector('main').firstElementChild);
+    const xfragmentBlock = decorateTeaser(xfragmentDOM.querySelector('main').firstElementChild);
     block.innerHTML = xfragmentBlock.outerHTML;
   }
 
